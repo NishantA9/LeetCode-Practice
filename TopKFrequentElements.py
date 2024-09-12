2dbaf286eddaba341740caee86c41a8108ffc269
NishantA9 <nishantacharekar12@gmail.com>
2024-09-12 10:04:16 -0400

    update TopKFrequentElements.py

--- TopKFrequentElements.py
+++ TopKFrequentElements.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         #This is a bucket sort solution
-        # Initialize a dictionary to count the frequency of each number in nums
+        # Initialize a dictionary/hashmap to count the frequency of each number in nums
         count = {}
         
         # Initialize a list of empty lists, where the index represents the frequency
