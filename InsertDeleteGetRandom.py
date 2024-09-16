712a4f7f642d14b38eea9d4d4a4802980d035672
NishantA9 <nishantacharekar12@gmail.com>
2024-09-16 18:09:52 -0400

    map

--- InsertDeleteGetRandom.py
+++ InsertDeleteGetRandom.py
@@ -3,16 +3,16 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        # Initialize an empty dictionary to store the values and their corresponding indices in the list
+        # Initialize an empty dictionary/hashmap to store the values and their corresponding indices in the list
         self.numMap = {}  
         # Initialize an empty list to store the values in the set
         self.numList = []  
 
     def insert(self, val: int) -> bool:
-        # Check if the value is not already in the set (i.e., not in the dictionary)
+        # Check if the value is not already in the set (i.e., not in the dictionary/hashmap)
         res = val not in self.numMap  
         if res:
-            # If the value is not in the set, add it to the dictionary with its index in the list
+            # If the value is not in the set, add it to the dictionary/hashmap with its index in the list
             self.numMap[val] = len(self.numList)  
             # Add the value to the end of the list
             self.numList.append(val)  
@@ -20,7 +20,7 @@ class RandomizedSet:
         return res  
 
     def remove(self, val: int) -> bool:
-        # Check if the value is in the set (i.e., in the dictionary)
+        # Check if the value is in the set (i.e., in the dictionary/hashmap)
         res = val in self.numMap  
         if res:
             # Get the index of the value in the list
@@ -31,9 +31,9 @@ class RandomizedSet:
             self.numList[idx] = lastVal  
             # Remove the last value from the list
             self.numList.pop()  
-            # Update the dictionary to reflect the new index of the last value
+            # Update the dictionary/hashmap to reflect the new index of the last value
             self.numMap[lastVal] = idx  
-            # Remove the value from the dictionary
+            # Remove the value from the dictionary/hashmap
             del self.numMap[val]  
         # Return True if the value was successfully removed, otherwise False
         return res  
