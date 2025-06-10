964b4f3fb6f7ccc1a3a30c26ded7c156faa8277f
NishantA9 <nishantacharekar12@gmail.com>
2025-06-10 19:25:40 -0400

    fgnw8

--- PalindromicSubstrings.py
+++ PalindromicSubstrings.py
@@ -1,4 +1,20 @@
 class Solution:
+    def countSubstrings(self, s: str) -> int: # TC: O(n^2) and SC: O(1)
+        res = 0  # To store the total count of palindromic substrings
+        def helper(l, r): # Helper function to expand around center and count palindromes #noqa: E741
+            nonlocal res
+            while l >= 0 and r < len(s) and s[l] == s[r]: # Expand while the characters at l and r are equal and in bounds
+                res += 1 # Valid palindrome found
+                l -= 1  # Expand to the left #noqa: E741
+                r += 1  # Expand to the right
+        for i in range(len(s)): # Try every possible center (both odd and even length)
+            helper(i, i) # Odd-length palindromes (center at i)
+            helper(i, i + 1) # Even-length palindromes (center between i and i+1)
+        return res  # Return the total number of palindromic substrings
+
+# another sol but outward function is used
+'''
+class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
         for i in range(len(s)):
@@ -12,3 +28,4 @@ class Solution:
             l -= 1  #noqa: E741
             r += 1
         return res
+'''
