c65c46c6e624d35c0970942c6f45fde964780080
NishantA9 <nishantacharekar12@gmail.com>
2025-07-10 23:39:41 -0400

    fg16

--- ConcatenationofArray.py
+++ ConcatenationofArray.py
@@ -6,3 +6,17 @@ class Solution:
             for num in nums:
                 ans.append(num)  # Append each element from nums
         return ans  # Final array: nums + nums
+    
+# another way to do it:
+class Solution2:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums * 2  # Concatenate the list with itself using multiplication or add operator like nums + nums
+    
+# what if you have k concatenations?
+class Solution3:
+    def getConcatenation(self, nums: List[int], k: int) -> List[int]:
+        ans = []  # Initialize the result list
+        for i in range(k):
+            for num in nums:
+                ans.append(num)
+        return ans
