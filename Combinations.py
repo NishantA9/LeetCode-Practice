17972334f3173cc99e79fd5e56a4f03027728391
NishantA9 <nishantacharekar12@gmail.com>
2025-10-14 20:49:26 -0500

    JSR23

--- Combinations.py
+++ Combinations.py
@@ -13,3 +13,20 @@ class Solution:
                 comb.pop()           # Backtrack: remove current number        
         backtrack(1, [])  # Start with number 1 and empty combination
         return res        # Return all generated combinations
+    
+class Solution2:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []  # Result list to store all combinations
+        def backtrack(i, comb):  # i: current number to consider, comb: current combination
+            if i > n:  # Base case: exceeded the range of numbers
+                if len(comb) == k:  # Check if we have exactly k elements
+                    res.append(comb.copy())  # Add valid combination to result
+                return           
+            # Decision 1: Include current number i in combination
+            comb.append(i)          # Add current number
+            backtrack(i + 1, comb)  # Recurse with next number
+            comb.pop()              # Backtrack: remove current number          
+            # Decision 2: Skip current number i (don't include it)
+            backtrack(i + 1, comb)  # Recurse with next number without including i        
+        backtrack(1, [])  # Start with number 1 and empty combination
+        return res        # Return all generated combinations
