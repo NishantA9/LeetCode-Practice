5cf10a5eadcaa50793f80410512360abce039ec3
NishantA9 <nishantacharekar12@gmail.com>
2026-01-01 22:29:12 -0600

    jsr52.1

--- LC-Problems/RemoveDuplicatesfromSortedArray.py
+++ LC-Problems/RemoveDuplicatesfromSortedArray.py
@@ -5,5 +5,5 @@ class Solution: #Two Pointers
         for r in range(1, len(nums)): # Iterate through the list starting from the second element (index 1)            
             if nums[r] != nums[r - 1]: # Check if the current element is different from the previous one             
                 nums[l] = nums[r] # If different, assign the current element to the position at 'l'                
-                l += 1 # Increment the left pointer                
+                l += 1 # Increment the left pointer    # noqa: E741            
         return l # Return the count of unique elements, which is the value of 'l'
