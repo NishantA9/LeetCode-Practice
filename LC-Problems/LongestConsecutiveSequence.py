547ea4b689bdffc13273abe1bb9ab73363dc34f6
NishantA9 <nishantacharekar12@gmail.com>
2026-01-28 00:04:39 -0600

    JSR54

--- LC-Problems/LongestConsecutiveSequence.py
+++ LC-Problems/LongestConsecutiveSequence.py
@@ -1,26 +1,12 @@
 from typing import List
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # Convert the list of numbers into a set to remove duplicates and allow O(1) lookups for the elements
-        numSet = set(nums)
-        
-        # Initialize a variable to keep track of the longest sequence length
-        longest = 0
-        
-        # Iterate over each number in the original list
-        for n in nums:
-            # Check if the current number is the start of a sequence (i.e., there is no number 'n-1' in the set)
-            if (n - 1) not in numSet:
-                # If it is the start, initialize the length of this sequence
-                length = 0
-                
-                # Continue checking for the next numbers in the sequence by incrementing length until the sequence breaks
-                while (n + length) in numSet:
-                    length += 1
-                
-                # Update the longest sequence found so far
-                longest = max(length, longest)
-        
-        # Return the length of the longest consecutive sequence
-        return longest
+        numSet = set(nums) # Convert the list of numbers into a set to remove duplicates and allow O(1) lookups for the elements
+        longest = 0   # Initialize a variable to keep track of the longest sequence length        
+        for n in nums:  # Iterate over each number in the original list
+            if (n - 1) not in numSet:  # Check if the current number is the start of a sequence (i.e., there is no number 'n-1' in the set)                
+                length = 0 # If it is the start, initialize the length of this sequence                                
+                while (n + length) in numSet: # Continue checking for the next numbers in the sequence by incrementing length until the sequence breaks
+                    length += 1                                
+                longest = max(length, longest) # Update the longest sequence found so far                
+        return longest # Return the length of the longest consecutive sequence
